c26217d76996df9a51e1dfdeaaeb6980a9f83c1e
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2019-01-19 15:55:23 +0530

    add simple bottom navigation

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter , Route , Switch } from 'react-router-dom';
 import Leaderboard from "./Leaderboard"
 import Login from "./Login"
+import bottomNavigation from './bottomNavigation';
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
               <div>
                 <Switch>
-                  <Route exact path="/leaderboard" component = {Leaderboard} />
+                  <Route exact path="/leaderboard" component = {bottomNavigation} />
                 </Switch>
               </div>
             </BrowserRouter>
