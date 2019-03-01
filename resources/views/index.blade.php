33238695fdc90bd827deaad7e989f872e7cddb4c
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2019-03-01 19:34:05 +0530

    change index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="https://spider.nitt.edu/mdecoder//images/favicon_temp.png">
+        <link rel="shortcut icon" href="{{asset('images/favicon_temp.png')}}">
         <title>Mdecoder2019</title>
         <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
     </head>
     <body>
         <div id="App"></div>
-        <script src="{{mix('mdecoder/js/app.js')}}"></script>
+        <script src="{{mix('/mdecoder/js/app.js')}}"></script>
     </body>
 </html>
